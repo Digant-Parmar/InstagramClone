30aa96f0356048fef59b2a201e309945f6e251ba
Digant-Parmar <digantparmar17@gmail.com>
2020-07-01 20:05:22 +0530

    SignUp and Login Parse, Fancy Toast

--- app/src/main/java/com/example/instagramclone/SingUp.java
+++ app/src/main/java/com/example/instagramclone/SingUp.java
@@ -4,30 +4,82 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
+import com.parse.SignUpCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 public class SingUp extends AppCompatActivity {
+
+    private EditText edtUserLogin, edtPassLogin, edtUserSignup, edtPassSignup;
+    private Button btnLogin,btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
 
-    public void helloWorldTapped(View view){
-        ParseObject boxer = new ParseObject("Boxer");
-        boxer.put("punch_speed",200);
-        boxer.saveInBackground(new SaveCallback() {
+        edtPassLogin = findViewById(R.id.edtPassLogin);
+        edtPassSignup = findViewById(R.id.edtPassSignup);
+        edtUserLogin = findViewById(R.id.edtUserLogin);
+        edtUserSignup = findViewById(R.id.edtUserSignup);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignup);
+
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void done(ParseException e) {
-                if(e==null){
-                    Toast.makeText(SingUp.this, "Boxer object saved", Toast.LENGTH_SHORT).show();
-                }
+            public void onClick(View v) {
+
+                final ParseUser parseUser = new ParseUser();
+                parseUser.setUsername(edtUserSignup.getText().toString());
+                parseUser.setPassword(edtPassSignup.getText().toString());
+
+                parseUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e==null){
+                            FancyToast.makeText(SingUp.this,parseUser.get("username")+" is signed up successfully",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+
+                        }else{
+                            FancyToast.makeText(SingUp.this,e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+
+                        }
+                    }
+                });
+
             }
         });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ParseUser.logInInBackground(edtUserLogin.getText().toString(), edtPassLogin.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser user, ParseException e) {
+                        if(user !=null && e==null){
+                            FancyToast.makeText(SingUp.this,user.get("username")+" is Logged in successfully",FancyToast.LENGTH_LONG,FancyToast.SUCCESS,true).show();
+
+                        }else {
+                            FancyToast.makeText(SingUp.this,e.getMessage(),FancyToast.LENGTH_LONG,FancyToast.ERROR,true).show();
+
+                        }
+                    }
+                });
+
+            }
+        });
+
     }
+
+
 }
